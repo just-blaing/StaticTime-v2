34aadb21025a21540bde024dd5d0cbe3b3a1cce4
blaing <127663348+just-blaing@users.noreply.github.com>
2025-12-07 21:03:44 +0300

    добавил умную хуйню

--- native.cs
+++ native.cs
@@ -1,13 +1,17 @@
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace statictime;
 
 public partial class MainWindow
 {
-    private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+    private delegate IntPtr LowLevelProc(int nCode, IntPtr wParam, IntPtr lParam);
     private const int WH_MOUSE_LL = 14;
+    private const int WH_KEYBOARD_LL = 13;
     private const int WM_MOUSEWHEEL = 0x020A;
+    private const int WM_KEYDOWN = 0x0100;
+
     [StructLayout(LayoutKind.Sequential)]
     private struct POINT
     {
@@ -23,11 +27,20 @@ public partial class MainWindow
         public uint time;
         public IntPtr dwExtraInfo;
     }
+    [StructLayout(LayoutKind.Sequential)]
+    private struct KBDLLHOOKSTRUCT
+    {
+        public uint vkCode;
+        public uint scanCode;
+        public uint flags;
+        public uint time;
+        public IntPtr dwExtraInfo;
+    }
     [DllImport("user32.dll")]
     public static extern short GetAsyncKeyState(int vKey);
 
     [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-    private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+    private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelProc lpfn, IntPtr hMod, uint dwThreadId);
 
     [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
@@ -37,14 +50,20 @@ public partial class MainWindow
     private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
 
     [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-
     private static extern IntPtr GetModuleHandle(string lpModuleName);
+    
+    [DllImport("user32.dll")]
+    private static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpKeyState, [Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pwszBuff, int cchBuff, uint wFlags);
+
+    [DllImport("user32.dll")]
+    private static extern bool GetKeyboardState(byte[] lpKeyState);
+    
     private const int VK_LBUTTON = 0x01;
     private const int VK_RBUTTON = 0x02;
     private const int VK_MENU = 0x12; 
     private const int VK_TAB = 0x09;
-
-    private IntPtr set_hook(LowLevelMouseProc proc)
+    private StringBuilder _word_buffer = new StringBuilder();
+    private IntPtr set_mouse_hook(LowLevelProc proc)
     {
         using (Process curProcess = Process.GetCurrentProcess())
         using (ProcessModule curModule = curProcess.MainModule!)
@@ -52,8 +71,17 @@ public partial class MainWindow
             return SetWindowsHookEx(WH_MOUSE_LL, proc, GetModuleHandle(curModule.ModuleName!), 0);
         }
     }
+    
+    private IntPtr set_kb_hook(LowLevelProc proc)
+    {
+        using (Process curProcess = Process.GetCurrentProcess())
+        using (ProcessModule curModule = curProcess.MainModule!)
+        {
+            return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName!), 0);
+        }
+    }
 
-    private IntPtr hook_callback(int nCode, IntPtr wParam, IntPtr lParam)
+    private IntPtr mouse_hook_callback(int nCode, IntPtr wParam, IntPtr lParam)
     {
         if (nCode >= 0 && wParam == (IntPtr)WM_MOUSEWHEEL)
         {
@@ -64,7 +92,45 @@ public partial class MainWindow
                 _tracker.data.total_scroll += 5;
             }
         }
-        return CallNextHookEx(_hook_id, nCode, wParam, lParam);
+        return CallNextHookEx(_mouse_hook_id, nCode, wParam, lParam);
+    }
+    
+    private IntPtr kb_hook_callback(int nCode, IntPtr wParam, IntPtr lParam)
+    {
+        if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+        {
+            KBDLLHOOKSTRUCT kb = Marshal.PtrToStructure<KBDLLHOOKSTRUCT>(lParam);
+            if (kb.vkCode == 0x20 || kb.vkCode == 0x0D) // Space or Enter
+            {
+                if (_word_buffer.Length > 0)
+                {
+                    _tracker.add_word(_word_buffer.ToString());
+                    _word_buffer.Clear();
+                }
+            }
+            else
+            {
+                byte[] keys = new byte[256];
+                if (GetKeyboardState(keys))
+                {
+                    StringBuilder buf = new StringBuilder(2);
+                    if (ToUnicode(kb.vkCode, kb.scanCode, keys, buf, buf.Capacity, 0) > 0)
+                    {
+                        char c = buf[0];
+                        if (char.IsLetterOrDigit(c) || c == '-')
+                        {
+                            _word_buffer.Append(c);
+                        }
+                        else if (_word_buffer.Length > 0)
+                        {
+                             _tracker.add_word(_word_buffer.ToString());
+                             _word_buffer.Clear();
+                        }
+                    }
+                }
+            }
+        }
+        return CallNextHookEx(_kb_hook_id, nCode, wParam, lParam);
     }
 
     private async Task input_loop(CancellationToken token)
